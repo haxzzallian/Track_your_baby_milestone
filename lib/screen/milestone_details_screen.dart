95be9b123da122e661ddd183b30a8a0cac46d083
haxzzallian <temitope.owolabi@outlook.com>
2023-12-13 19:00:20 +0100

    edited

--- lib/screen/milestone_details_screen.dart
+++ lib/screen/milestone_details_screen.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/milestones.dart';
 
 class MilestoneDetailsScreen extends StatelessWidget {
-  const MilestoneDetailsScreen({Key key}) : super(key: key);
+  static const routeName = '/milestone-detail';
+  final String type;
+  final String remark;
+  DateTime milestoneDate;
+  final String id;
+
+  MilestoneDetailsScreen({this.type, this.milestoneDate, this.remark, this.id});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    /*final milestoneId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
+    final loadedMileStone = Provider.of<Milestones>(
+      context,
+      listen: false,
+    ).findById(milestoneId);*/
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(type),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+                height: 300,
+                width: double.infinity,
+                child: Text('milestoneDate')),
+            SizedBox(height: 10),
+            Text(
+              type,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                remark,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
